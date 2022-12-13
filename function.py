916e6bb6f5d6ef6e54e6dd9d73ba27f5667fdee9
nitin3778 <nitinchityal3778@gmail.com>
2022-12-13 14:37:52 +0530

    my first commit

---
+++ function.py
@@ -0,0 +1,7 @@
+
+print('This is my file')
+def addition(a, b):
+    return a+b
+
+
+print(addition(5, 4))
---
+++ test.py
@@ -0,0 +1,5 @@
+import function
+
+print('This is my test file')
+
+print(function.addition(2,3))
